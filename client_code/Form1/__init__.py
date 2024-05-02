b4d668ac4fbf3b54692282353fb97b3288b38dbc
rajaramvg@arkaniallc.com <rajaramvg@arkaniallc.com>
2024-05-02 05:57:09 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,7 +6,7 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.check_boxes = [self.cb1, self.cb2, self.cb3, self.cb4, self.cb5, self.cb6]
     # Any code you write here will run before the form opens.
 
   def store_data(current_country, languages, citizenship):
@@ -19,8 +19,14 @@ class Form1(Form1Template):
     # Selected value from drop down is stored in current country field
     current_country = self.current_drpdwn.selected_value
     
-    """This method is called when the button is clicked"""
-    pass
+    # Get the list of all langauges spoken
+    for cb in self.check_boxes:
+      if cb.checked == True:
+        languages = languages + cb.text
+    
+    # Next vacation plan motivation
+    vacation = self.radio_button_1
+
     
 
     
